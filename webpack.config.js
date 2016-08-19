a631ed054bc0faa4a9285cbe77dd4d22e1cfbfab
stasyaner <stasyaner@hotmail.com>
2016-08-19 21:34:07 +0300

    initial settings

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry:  './index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel?cacheDirectory']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        loaders: ['style-loader', 'css-loader', 'less-loader']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+      ,
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  resolve:{
+    extensions: ['', '.js', '.jsx', '.less', '.css', '.scss']
+  },
+  devServer: {
+    // contentBase: './public',
+    // colors: true,
+    historyApiFallback: true, // !!this is rly needed for HTML5 history api and react router
+    // inline: true,
+    // proxy: {
+    //   '/xlsxExport': {
+    //     target: 'ws://localhost:3000',
+    //     secure: false,
+    //     ws: true
+    //   },
+    // },
+  }
+}
+
+/*
+ * If bundling for production, optimize output
+ */
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = false;
+  config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    })
+  ];
+};
